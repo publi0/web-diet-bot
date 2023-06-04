2c8a11a6a33a7b67df315c8a50c8e1176e0a4aa5
Felipe Públio <felipe@publio.dev>
2023-06-04 15:44:50 -0300

    feature - add go mod

--- app/main.go
+++ app/main.go
@@ -12,12 +12,12 @@ func main() {
 }
 
 func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
-	fmt.Printf("Body = %s", request.Body)
+	fmt.Printf("Processing request data for request %s. \n", request.RequestContext.RequestID)
+	fmt.Println("Body = ", request.Body)
 
-	fmt.Println("Headers:")
+	fmt.Println("\n Headers:")
 	for key, value := range request.Headers {
-		fmt.Printf("    %s: %s\n", key, value)
+		fmt.Printf("    %s: %s - ", key, value)
 	}
 
 	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
